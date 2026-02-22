525288af21cc9ae15e077cf46c7bf9e8e9986f48
Ahmar Abbas <94463585+syedahmarabbas@users.noreply.github.com>
2026-02-22 15:07:26 +0100

    Added CORS

--- gen_backend/main.py
+++ gen_backend/main.py
@@ -3,8 +3,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from gen_backend.routes import router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
